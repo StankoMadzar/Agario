250ecb29de65182575187b9e9c6403915c7f5557
Stanko Madzar <stanko.madzar@outlook.com>
2026-03-23 18:25:57 +0100

    Initial commit for Agario project

---
+++ Agario/Agario.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
